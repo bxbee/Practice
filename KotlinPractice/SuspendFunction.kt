9837c73f638e1fd81fa0a4d4d5055a043e3223e4
Raj Ghosh <148113238+bxbee@users.noreply.github.com>
2026-04-09 12:57:59 +0530

    Implement suspend function for breakfast preparation
    Added a suspend function to simulate making breakfast asynchronously.

--- KotlinPractice/SuspendFunction.kt
+++ KotlinPractice/SuspendFunction.kt
@@ -1,1 +1,21 @@
 
+import kotlinx.coroutines.*
+
+class SuspendFunction {
+    suspend fun makeBreakfast(): String {
+        println("Starting breakfast")
+        delay(4000) // suspends for 2 seconds
+        return "Breakfast is Ready!"
+    }
+}
+
+fun main() = runBlocking {
+    println("Good Morning")
+
+    launch {
+        val breakfast = SuspendFunction().makeBreakfast()
+        println(breakfast)
+    }
+
+    println("I can do other things while breakfast is being made!")
+}
