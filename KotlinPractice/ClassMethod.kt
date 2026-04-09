8ddf1f4487bbb0409dfe9990364e161b28c35d2f
Raj Ghosh <148113238+bxbee@users.noreply.github.com>
2026-04-09 12:04:36 +0530

    Add function to class for Practice

--- KotlinPractice/ClassMethod.kt
+++ KotlinPractice/ClassMethod.kt
@@ -1,1 +1,15 @@
+class function{
+  fun greet(name:String):String{
+    return ("Hello $name")
+    
+    
+  }
+}
 
+fun main(){
+  val MyFunction = function()
+  val result = MyFunction.greet("REX")
+  println(result)
+  
+}
+  
